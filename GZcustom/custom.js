9f75d32b810eeefb076d851d1ad34ce69908a687
GarytheNoob <GarytheNoob@users.noreply.github.com>
2023-01-15 06:57:04 +0000

    deploy: 12d85be7b07bbb4257be2e6871fea2eefb3bb0ed

--- GZcustom/custom.js
+++ GZcustom/custom.js
@@ -3,11 +3,9 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function() {
 	if (document.hidden) {
-		$('[rel="icon"]').attr('href', "/funny.ico");
 		document.title = '==Page Crashed==';
 		clearTimeout(titleTime);
 	} else {
-		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
 		document.title = 'Recovered! ' + OriginTitle;
 		titleTime = setTimeout(function() {
 			document.title = OriginTitle;
