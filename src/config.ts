6bb304cdac738dea9018108e7461178394404035
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-06-26 16:41:57 +0800

    basic structure and configurations

--- src/config.ts
+++ src/config.ts
@@ -9,20 +9,20 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
   title: "Gary's Blog",
-  subtitle: "",
+  subtitle: "Sharing random things",
   lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
   themeColor: {
-    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false, // Hide the theme color picker for visitors
+    hue: 260, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true, // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: "assets/images/numazu_firework.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false, // Display the credit text of the banner image
-      text: "", // Credit text to be displayed
-      url: "", // (Optional) URL link to the original artwork or artist's page
+      enable: true, // Display the credit text of the banner image
+      text: "提供：沼津市", // Credit text to be displayed
+      url: "https://www.city.numazu.shizuoka.jp/photolibrary/", // (Optional) URL link to the original artwork or artist's page
     },
   },
   toc: {
@@ -46,33 +46,28 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: "GitHub",
-      url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+      url: "https://github.com/GarytheNoob", // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
     },
   ],
 };
 
 export const profileConfig: ProfileConfig = {
-  avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: "assets/images/avatar.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: "GarytheNoob",
-  bio: "跨过大海寻找下一道光辉",
+  bio: "二偶吃，偶尔整点有的没的",
   links: [
-    {
-      name: "Twitter",
-      icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-      // You will need to install the corresponding icon set if it's not already included
-      // `pnpm add @iconify-json/<icon-set-name>`
-      url: "https://twitter.com",
-    },
-    {
-      name: "Steam",
-      icon: "fa6-brands:steam",
-      url: "https://store.steampowered.com",
-    },
     {
       name: "GitHub",
       icon: "fa6-brands:github",
       url: "https://github.com/GarytheNoob",
+    },
+    {
+      name: "Twitter",
+      icon: "ic:baseline-email", // Visit https://icones.js.org/ for icon codes
+      // You will need to install the corresponding icon set if it's not already included
+      // `pnpm add @iconify-json/<icon-set-name>`
+      url: "mailto:garythenoob@outlook.com",
     },
   ],
 };
